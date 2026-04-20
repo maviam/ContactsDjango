0fbc683ede54525c60003e003285081dea3fcd95
Marcelo Amorim <marceloamorim.fa@gmail.com>
2026-04-20 19:47:15 +0100

    Single Contact

--- contact/admin.py
+++ contact/admin.py
@@ -4,7 +4,7 @@ from contact import models
 # Register your models here.
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id','first_name','last_name','mobile_phone')
+    list_display = ('id','first_name','last_name','mobile_phone','show')
     ordering = ('-first_name',)
     list_filter = ('created_date',)
     search_fields = ('first_name','mobile_phone')
--- contact/views.py
+++ contact/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from contact.models import Contact
 
 # Create your views here.
 def index(request):
+    contacts = Contact.objects.filter(show=True).order_by("-id")[0:10]
+    context = {
+        'contacts': contacts,
+        'page_title': 'Contacts List'
+    }
     return render(
         request,
-        'contact/index.html'
+        'contact/index.html',
+        context
+    )
+
+
+# See a specific contact
+def contact(request, contact_id):
+    single_contact = get_object_or_404(Contact.objects, id=contact_id, show=True)
+    context = {
+        'contact': single_contact,
+        'page_title': f"Contact no. {contact_id}"
+    }
+    return render(
+        request,
+        'contact/contact.html',
+        context
     )
