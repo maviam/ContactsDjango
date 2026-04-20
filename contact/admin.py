95546a3cbd1c07fd4abafb5b803996db2cb9a45f
Marcelo Amorim <marceloamorim.fa@gmail.com>
2026-04-20 09:51:31 +0100

    Model and Administration - Before connection with database

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from contact import models
 
 # Register your models here.
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id','first_name','last_name','mobile_phone')
+    ordering = ('-first_name',)
+    list_filter = ('created_date',)
+    search_fields = ('first_name','mobile_phone')
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    mobile_phone = models.CharField(max_length=9)
+    email = models.EmailField(max_length=100, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.email})"
+    
+    
